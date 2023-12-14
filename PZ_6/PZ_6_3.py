1641e752f576adacfe34252a82676d5b75bc5566
shatoumx <art.mirqn@yandex.ru>
2023-12-14 21:27:05 +0300

    upd
    upd

--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -1,20 +1,33 @@
+# Дано множество А из N точек на плоскости и точка В (точки заданы своими
+# координатами х, у). Найти точку из множества А, наиболее близкую к точке В.
+# Расстожше R между точками с координатами (XI, у) и (м, у) вычисляется по
+# формуле:
+# R = √(х2 - х1):2 + (у2 - у1)^2.
+# Для хранения данных о каждом наборе точек следует использовать по два списка: первый
+# список для хранения абсцисс, второй — для хранения ординат.
 import math
 
-def find_closest_point(points):
-    closest_distance = float('inf')
-    closest_point = (0, 0)
 
-    for i in range(len(points[0])):
-        x = points[0][i]
-        y = points[1][i]
+def find_closest_point(A):
+    closest_point = [0, 0]  # Изначально считаем точку с нулевыми координатами ближайшей
+    closest_distance = math.inf  # Изначально считаем бесконечную дистанцию
 
-        if x >= 0 and y >= 0:
-            distance = math.sqrt(x2 + y2)
+    for i in range(len(A[0])):
+        x = A[0][i]
+        y = A[1][i]
+
+        # Проверяем, что точка лежит в первой или третьей четверти
+        if x >= 0 and y >= 0 or x <= 0 and y <= 0:
+            distance = math.sqrt(x ** 2 + y ** 2)  # Вычисляем расстояние до начала координат
+
             if distance < closest_distance:
                 closest_distance = distance
-                closest_point = (x, y)
+                closest_point = [x, y]
 
-    if closest_distance == float('inf'):
-        return (0, 0)
-    else:
-        return closest_point
+    return closest_point
+
+
+# Пример использования функции
+A = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+closest_point = find_closest_point(A)
+print("Ближайшая точка:", closest_point)
