a0769e184ab270d5cf2da94af801090a29da64a2
shatoumx <art.mirqn@yandex.ru>
2023-12-14 22:47:45 +0300

    upd
    upd

---
+++ PZ_7/PZ_7_2.py
@@ -0,0 +1,17 @@
+# Дана строка, состоящая из русских слов, разделенных пробелами (одним или несколькими). Найти длину самого длинного
+# слова.
+def find_longest_word_length(string):
+    words = string.split()  # Разделяем строку на слова по пробелам
+    longest_word_length = 0  # Переменная для хранения длины самого длинного слова
+
+    for word in words:
+        if len(word) > longest_word_length:
+            longest_word_length = len(word)
+
+    return longest_word_length
+
+
+# Пример использования
+input_string = "ааааааааа"
+result = find_longest_word_length(input_string)
+print(result)
